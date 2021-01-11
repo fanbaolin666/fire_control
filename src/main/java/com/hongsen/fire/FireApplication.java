9fb4289c80225a20f2c088d086067731322185b5
fanbaolin <f13683758197@163.com>
2021-01-11 16:35:23 +0800

    测试提交

--- src/main/java/com/hongsen/fire/FireApplication.java
+++ src/main/java/com/hongsen/fire/FireApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FireApplication {
+public class FireApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(FireApplication.class, args);
---
+++ src/main/java/com/hongsen/fire/aaa.java
@@ -0,0 +1,10 @@
+package com.hongsen.fire;
+
+/**
+ * @program: fire_control
+ * @description:
+ * @author: fbl
+ * @create: 2021-01-11 16:34
+ **/
+public class aaa {
+}
