4d32a09b63b66b4083a2e7f1980ad4006cf578de
fanbaolin <f13683758197@163.com>
2021-01-11 16:40:28 +0800

    测试提交

--- src/main/java/com/hongsen/fire/FireApplication.java
+++ src/main/java/com/hongsen/fire/FireApplication.java
@@ -3,8 +3,11 @@ package com.hongsen.fire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
-public class FireApplication  {
+public class FireApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(FireApplication.class, args);
