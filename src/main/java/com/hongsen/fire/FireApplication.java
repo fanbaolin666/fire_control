2eaf573d610b4c2653a4577b355efd960c51656b
huajian <287643967@qq.com>
2021-01-11 16:49:22 +0800

    华舰测试

--- src/main/java/com/hongsen/fire/FireApplication.java
+++ src/main/java/com/hongsen/fire/FireApplication.java
@@ -11,6 +11,7 @@ public class FireApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(FireApplication.class, args);
+        System.out.println(123);
     }
 
 }
