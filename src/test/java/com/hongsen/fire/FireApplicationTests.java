67b50cee9e021d4baa1ca6d4f78adfef8532494c
fanbaolin <f13683758197@163.com>
2021-01-11 16:32:45 +0800

    Initial commit

---
+++ src/test/java/com/hongsen/fire/FireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongsen.fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
